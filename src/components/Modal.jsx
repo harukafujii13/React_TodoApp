665e31daa27862bde73592e4a142e05948a06303
Haruka <hfujii1013@gmail.com>
2023-04-07 13:09:52 -0700

    added memo

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,6 +6,7 @@ function Modal() {
     const [showModal, setShowModal] = useState(false)
 
     const modalRef = useRef()
+    //used to reference the DOM element of the modal
 
     useClickOutside(modalRef, showModal, () => setShowModal(false))
 
@@ -13,7 +14,7 @@ function Modal() {
         <div>
             <button onClick={() => setShowModal(true)}>Modal</button>
             {showModal && (
-                <div className={styles.overlay}>
+                <div className={styles.overlay}>  {/* provides a semi-transparent background. */}
                     <div
                         ref={modalRef}
                         className={styles.modal}
@@ -31,3 +32,15 @@ function Modal() {
 }
 
 export default Modal
+
+//memo1
+//useClickOutside hook to handle clicks outside the modal. 
+//The useClickOutside hook takes three arguments: the ref of the element to track for clicks outside (in this case, modalRef), 
+//the current value of showModal state variable, and a callback function to be called 
+//when a click outside the element is detected. The callback function, () => setShowModal(false), 
+//is passed as the third argument, and it sets the showModal state to false when a click outside the modal is detected.
+
+//memo2
+//The modal content is wrapped in a div element with the class name styles.modal, 
+//and the modalRef is attached to it using the ref attribute, allowing it to be referenced 
+//in the custom useClickOutside hook.
--- src/hooks/useClickOutside.jsx
+++ src/hooks/useClickOutside.jsx
@@ -1,6 +1,11 @@
 import { useEffect } from 'react'
 
-export const useClickOutside = (ref, currentState, updater) => {
+export const useClickOutside = (ref, currentState, updater) => { 
+
+    //ref: A ref object that represents the DOM element to track for clicks outside.
+    //currentState: The current state of a boolean variable that determines 
+    //              whether the component associated with the hook should be in an active or inactive state.
+    //updater: A callback function to be called when a click outside the tracked DOM element is detected.
 
     useEffect(() => {
         const handler = (e) => {
@@ -9,11 +14,23 @@ export const useClickOutside = (ref, currentState, updater) => {
             }
         }
 
+        //The handler function checks if currentState is truthy (i.e., true), 
+        //if ref.current is defined and not null, and if the target of the mousedown event (e.target) 
+        //is not contained within the ref.current element using the contains method. 
+        //If these conditions are met, it calls the updater callback function
+
         document.addEventListener("mousedown", handler)
 
         return () => {
-            document.removeEventListener("mousedown", handler)
+            document.removeEventListener("mousedown", handler) //cleanup function
         }
     }, [ref, currentState, updater])
 
 }
+
+//memo1
+//The useClickOutside custom hook in the given code is designed to handle clicks outside a specified DOM element. 
+//It uses the useEffect hook from React to set up an event listener for mousedown events on the document, 
+//and then removes the event listener when the component unmounts or when the dependencies of the effect change.
+//The useClickOutside custom hook in the given code is designed to handle clicks outside a specified DOM element. 
+//It uses the useEffect hook from React to set up an event listener for mousedown events on the document, and then removes the event listener when the component unmounts or when the dependencies of the effect change.
